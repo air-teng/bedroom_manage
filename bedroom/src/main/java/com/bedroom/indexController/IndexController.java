28f6097984fe1eaa9a4d97fc6f85ac279c657789
gitchuanone <1192685855@qq.com>
2019-05-19 18:09:56 +0800

    [HTML展示修改]  :  包导入修改成功

---
+++ bedroom/src/main/java/com/bedroom/indexController/IndexController.java
@@ -0,0 +1,14 @@
+package com.bedroom.indexController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("{pageName}")
+	public String pageName(@PathVariable("pageName")String pageName) {
+		return pageName;
+	}
+}
