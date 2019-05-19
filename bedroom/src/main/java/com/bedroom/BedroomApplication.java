bb82a67a40e0c31c162c112cde100bc7c96246e5
gitchuanone <1192685855@qq.com>
2019-05-19 17:14:16 +0800

    [模板导入] : html的css和js的thymeleaf导入修改

---
+++ bedroom/src/main/java/com/bedroom/BedroomApplication.java
@@ -0,0 +1,13 @@
+package com.bedroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BedroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BedroomApplication.class, args);
+	}
+
+}
